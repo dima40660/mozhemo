7b17cad1639b260796d9aafa965747c0d90faccb
Dmytro Shcherbyna <dima40660@gmail.com>
2023-01-23 23:30:11 +0200

    fix dockerfile, add nginx for prod

--- deployment.sh
+++ deployment.sh
@@ -3,4 +3,5 @@ git pull origin main
 docker container stop mozhemo-frontend
 docker container rm mozhemo-frontend
 docker image build -t mozhemo/frontend .
-docker container run -itd --name mozhemo-frontend mozhemo/frontend
+
+docker run -d --name mozhemo-frontend -p 80:80 mozhemo/frontend
